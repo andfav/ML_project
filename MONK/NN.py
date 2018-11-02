9bb22b3d7ef3340aec3bccb899147575ae423267
andfav <32015309+andfav@users.noreply.github.com>
2018-11-02 17:35:13 +0100

    Modificato NN:py
    Aggiunto il supporto strutturale (non learn) al deep learning e ovviato al problema del passaggio della NeuralNetwork ad ogni Unit.

--- MONK/NN.py
+++ MONK/NN.py
@@ -2,6 +2,7 @@
 Classi Unit e NeuralNetwork che implementano il nostro modello di rete neurale,
 in particolare è utilizzato un algoritmo di apprendimento backpropagation di tipo
 batch.
+Le classi sono predisposte anche al deep learning, sebbene il learn non lo sia.
 """
 from Input import Attribute, Input, TRInput
 
@@ -10,16 +11,16 @@ from Input import Attribute, Input, TRInput
 # distinto nei due casi sopra citati) si demanda ad opportune sottoclassi.
 class Unit(object):
 
-    def __init__(self, pos : int, dim : int, ValMax : float, network : object, f):
+    def __init__(self, pos : int, dim : int, ValMax : float, prevLayer : list, f):
         from random import uniform
         #Inizializzazione dei pesi a valori casuali (distr. uniforme).
         self.weights = [uniform(0,ValMax) for i in range(dim)]
 
-        #Memorizzazione del layer corrente, del numero di connessioni al layer precedente,
-        #della rete neurale relativa all'unità, della funzione di attivazione.
+        #Memorizzazione della posizione nel layer corrente, del numero di connessioni 
+        # al layer precedente, di copia del layer (lista) precedente, della funzione di attivazione.
         self.pos = pos
         self.dim = dim
-        self.net = network
+        self.prevLayer = prevLayer
         self.activation = f
     
     #Restituisce il Net (somma pesata dei valori in ingresso all'unità) calcolato a partire
@@ -27,12 +28,14 @@ class Unit(object):
     def getNet(self, inp: Input):
         s = 0
         if self.pos == 1:
-            for i in range(inp.getLength()):
-                i = self.net.getLayer(0).index(inp)
-                s += self.weights[i]*inp.getValue(i)
+            if inp.getLength() == self.dim:
+                for i in range(inp.getLength()):
+                    s += self.weights[i]*inp.getValue(i)
+            else:
+                raise ValueError ("Inserted input is not valid for this NN!")
         else:
-            for unit in self.net.getLayer(self.pos-1):
-                i = self.net.getLayer(self.pos-1).index(unit)
+            for unit in self.prevLayer:
+                i = self.prevLayer.index(unit)
                 s += self.weights[i]*unit.getOutput(inp)
         return s
 
@@ -46,9 +49,21 @@ class HiddenUnit(Unit):
 
     def __init__(self, pos, dim, ValMax, network, f):
         super().__init__(pos,dim,ValMax,network,f)
+
+        # Inizializzo il layer successivo necessario al calcolo del delta.
+        self.succLayer = []
+
+    #Metodo che aggiunge il layer successivo. Per il fatto che il layer
+    #successivo non esiste ancora in fase di inizializzazione dell'unità corrente
+    #risulta necessario fare tale aggiunta in un momento successivo.
+    def addSuccLayer(self, succLayer):
+        self.succLayer = succLayer
     
     def getDelta(self):
-        raise NotImplementedError
+        if len(self.succLayer) > 0:
+            raise NotImplementedError
+        else:
+            raise RuntimeError ("A hidden unit has not constructed correctly: succLayer missing!")
 
 #Sottoclasse delle unità di output.
 class OutputUnit(Unit):
@@ -70,8 +85,9 @@ class NeuralNetwork(object):
                     'alpha':       0.1,
                     'lambda':      0.1,
                     'ValMax':      0.2,
-                    'HiddenUnits': 2,
-                    'OutputUnits': 1, 
+                    'HiddenLayers': 1,
+                    'HiddenUnits':  2,
+                    'OutputUnits':  1, 
                     'ActivFun': lambda x: 1/(1+(exp(-x)))}
 
         #Aggiornamento degli iperparametri.
@@ -81,10 +97,11 @@ class NeuralNetwork(object):
             else:
                 raise ValueError ("new_hyp must be a subdict of hyp!")
 
-        #Lista dei layers.
+        #Lista dei layers e numero degli hidden layers.
         self.layers = []
+        self.noHiddLayers = self.hyp['HiddenLayers']
 
-        #Inserimento in input del training set.
+        #Inserimento in del training set.
         if len(trainingSet) == 0 or not isinstance(trainingSet[0], TRInput):
             raise ValueError ("inserted TR set is not valid!")
         else:
@@ -95,20 +112,39 @@ class NeuralNetwork(object):
             self.layers.append(trainingSet.copy())
 
         #Creazione delle hidden units.
+        #Costruzione del primo hidden layer: assumiamo che prevLayer = [] (il metodo getNet controlla
+        # infatti pos e assume un comportamento diverso qualora pos==1).
         hiddenList = []
         for i in range(self.hyp['HiddenUnits']):
-                hiddenList.append(HiddenUnit(1,length,self.hyp['ValMax'],self,self.hyp['ActivFun']))
+            hiddenList.append(HiddenUnit(1,length,self.hyp['ValMax'],[],self.hyp['ActivFun']))
         self.layers.append(hiddenList)
+        length = len(hiddenList)
+        #Costruzione degli hidden layers successivi (se esistono).
+        for j in range(1,self.noHiddLayers):
+            hiddenList = []
+            for i in range(self.hyp['HiddenUnits']):
+                hiddenList.append(HiddenUnit(j+1,length,self.hyp['ValMax'],self.getLayer(j),self.hyp['ActivFun']))
+            self.layers.append(hiddenList)
+            length = len(hiddenList)
+            #Inserimento del succLayer nelle unità del layer precedente.
+            for unit in self.getLayer(j):
+                unit.addSuccLayer(self.getLayer(j+1))
 
         #Creazione delle output units.
         outputList = []
         for i in range(self.hyp['OutputUnits']):
-                outputList.append(OutputUnit(2,len(hiddenList),self.hyp['ValMax'],self,self.hyp['ActivFun']))
+                outputList.append(OutputUnit(self.noHiddLayers+1,length,self.hyp['ValMax'],self.getLayer(self.noHiddLayers),self.hyp['ActivFun']))
         self.layers.append(outputList)
+        #Inserimento del succLayer nelle unità del layer precedente (hidden).
+        for unit in self.getLayer(self.noHiddLayers):
+            unit.addSuccLayer(self.getLayer(self.noHiddLayers+1))
 
     #Esegue backpropagation e istruisce la rete settando i pesi.
     def learn(self):
-        raise NotImplementedError
+        if self.noHiddLayers == 1:
+            raise NotImplementedError
+        else:
+            raise NotImplementedError ("Deep learning models not already implemented.")
     
     #Restituisce copia della lista rappresentante il layer i-esimo.
     def getLayer(self,i):
@@ -120,7 +156,7 @@ class NeuralNetwork(object):
     #Restituisce la lista degli output di rete dato un input.
     def getOutput(self, inp : Input):
         out_list = []
-        for outnode in self.layers[2]:
+        for outnode in self.layers[len(self.layers)-1]:
             out_list.append(outnode.getOutput(inp))
         return out_list
     
@@ -155,10 +191,14 @@ i1=TRInput([a1,a2],False)
 
 a1=Attribute(5,1)
 a2=Attribute(4,3)
+a3=Attribute(2,1)
 i2=TRInput([a1,a2],True)
+i3=Input([a1,a2,a3])
 
 il=[i1,i2]
 n = NeuralNetwork(il)
-print(n.getOutput(i1))
+print(n.getOutput(i2))
 print(n.getError([i2],0,1))
+#ValueError: Inserted input is not valid for this NN!
+#n.getOutput(i3)
 

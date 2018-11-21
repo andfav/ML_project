60c218c450b341b96aa92aa9a5c1f6ad66914a97
MrMostarda <matteo.anselmi93@gmail.com>
2018-11-21 12:29:30 +0100

    Add files via upload

--- MONK/Input.py
+++ MONK/Input.py
@@ -1,11 +1,21 @@
 """
+Classe in cui l'attributo può assumere anche valori continui
+"""
+class Attribute(object):
+    def __init__(self, value):
+        self.value = value
+
+    def getValue(self):
+        return self.value
+
+"""
 Questa classe modella il concetto di attributo 
 corrispondente ad uno specifico input. Chiamata 'N' la cardinalità del dominio dell'
 attributo , il valore che un attributo può assumere va da 1 a N, quindi se avremo un 
 attributo che può assumere i seguenti valori {"blue", "green", "red"}, il valore 1
 corrisponderà a "blue", il valore 2 a "green" e il valore 3 a "red"
 """
-class Attribute(object):
+class OneOfKAttribute(Attribute):
 
     #cardinality: cardinalità del dominio dell'attributo
     #value: valore da assegnare all'attributo
@@ -35,7 +45,7 @@ class Attribute(object):
             if self.represention[i]:
                 value = i + 1
             i += 1
-        return Attribute(cardinality, value)
+        return OneOfKAttribute(cardinality, value)
 
     #restituisce il valore corrispondente a value all'interno del dominio
     #es. se representation = 010 ==> Decode(["blue", "green", "red"]) restituisce la stringa "green"
@@ -67,13 +77,11 @@ class Attribute(object):
                 string += str(0)
         print(string)
 
-    #restituisce la rappresentaziione come lista di interi
-    def toInt(self):
-        l = list()
-        for elem in self.represention:
-            l.append(int(elem))
-        return l
+   
 
+"""
+Input i cui attributi possono asumere valori continui
+"""
 class Input(object):
 
     def __init__(self, attributeList: list):
@@ -84,17 +92,60 @@ class Input(object):
 
         for attribute in attributeList:
             if isinstance(attribute, Attribute):
+                self.vector.append(attribute)
+                
+            else:
+                raise ValueError ("attributeList must be a list of element of class Attribute")
+    
+    #interfacing: restituisce il numero di attributi dell'Input corrente
+    def len(self):
+        return len(self.vector)
+
+    #interfacing: restituisce il valore dell'attributo i-esimo
+    def getValue(self, i: int):
+        if i >= 0 and i < self.len():
+            return self.vector[i].value()
+        else:
+            raise ValueError("Index i out of bounds")
+
+    def print(self):
+        string = ""
+        i = 0
+        while i < len(self.vector -1):
+            string+= str(self.vector[i].value())+ " "
+            i += 1
+        string+= str(self.vector[i].value)
+        print(string)
+
+    def copy(self):
+        l = list
+        for attr in self.vector:
+            l.append(attr.copy())
+        return Input(l)
+"""
+Input i cui attributi sono codificati in modo 1-of-k
+"""
+class OneOfKInput(Input):
+
+    def __init__(self, attributeList: list):
+        if len(attributeList) <= 0:
+            raise ValueError ("length of attribute list must be greater than 0")
+
+        self.vector = list()
+
+        for attribute in attributeList:
+            if isinstance(attribute, OneOfKAttribute):
                 i = 0
                 check = False
                 while i < len(attribute.represention) and not check:
                     if attribute.represention[i]:
-                        attr = Attribute(len(attribute.represention), i+1)
+                        attr = OneOfKAttribute(len(attribute.represention), i+1)
                         check = True
                         self.vector.append(attr)
                     i += 1
                 
             else:
-                raise ValueError ("attributeList must be a list of element of class Attribute")
+                raise ValueError ("attributeList must be a list of element of class OneOfKAttribute")
     
     #interfacing: restituisce il numero di variabili 0-1 distinte dell'Input corrente
     def getLength(self):
@@ -106,12 +157,6 @@ class Input(object):
     def len(self):
         return len(self.vector)
 
-    #retituisce una lista di interi che è la codifica 1-of-k di tutti gli attributi dell'input
-    def toInt(self):
-        l = list()
-        for attr in self.vector:
-            l.append(attr.toInt())
-        return l
 
     #interfacing: restituisce il valore 0-1 memorizzato in posizione i dell'Input corrente
     #ACHTUNG: i in (0,length-1)!!
@@ -139,7 +184,7 @@ class Input(object):
         for att in self.vector:
             attrList.append(att.copy())
 
-        return Input(attrList)
+        return OneOfKInput(attrList)
 
     #get the attribute in position i inside vector
     def get(self, index:int):
@@ -155,8 +200,36 @@ class Input(object):
             l = l + attribute.copy().represention
         return l
 
-#Sottoclasse degli input completi di target: ideali per TR, VS.
+"""
+Sottoclasse degli input completi di target: ideali per TR, VS.
+"""
 class TRInput(Input):
+    def __init__(self, attributeList: list, target: bool):
+        super().__init__(attributeList)
+        self.target = target
+
+    #interfacing: restituisce il target associato all'input corrente.
+    def getTarget(self):
+        return self.target
+
+    def print(self):
+        super().print()
+        print("target "+ str(self.target))
+
+    def len(self):
+        return len(self.vector)
+            
+    def copy(self):
+        attrList = list()
+        for att in self.vector:     
+            attrList.append(att.copy())
+
+        return TRInput(attrList, self.target)
+
+"""
+Sottoclasse degli input completi di target: ideali per TR, VS.
+"""
+class OneOfKTRInput(OneOfKInput):
     def __init__(self, attributeList: list, target: bool):
         super().__init__(attributeList)
         self.target = target
@@ -177,28 +250,27 @@ class TRInput(Input):
             
             attrList.append(att.copy())
 
-        return TRInput(attrList, self.target)
+        return OneOfKTRInput(attrList, self.target)
 
     def len(self):
         return len(self.vector)
 
-    
-            
+
 
 
 
 #{"blue", "green", "red"}
-a1 = Attribute(3, 2)
+a1 = OneOfKAttribute(3, 2)
 print(str(a1.decode(["blue", "green", "red"])))
 
 
 #{-1, 0, 1, 2}
-a2 = Attribute(4, 3)
+a2 = OneOfKAttribute(4, 3)
 print(str(a2.decode([-1, 0, 1, 2])))
 
 attrList = [a1, a2]
 
-input1 = Input(attrList)
+input1 = OneOfKInput(attrList)
 
 input1.print()
 
@@ -210,7 +282,7 @@ print(input1.getValue(4)==0)
 print(input1.getValue(5)==1)
 print(input1.getValue(6)==1)
 
-input2 = TRInput(attrList,True)
+input2 = OneOfKTRInput(attrList,True)
 input2.print()
 
 

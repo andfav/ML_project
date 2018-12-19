a74d9965e6a7e13561434f312929994c1fea57dc
MrMostarda <matteo.anselmi93@gmail.com>
2018-12-19 16:29:12 +0100

    Add files via upload

---
+++ MONK/ValidationParallelo.py
@@ -0,0 +1,69 @@
+from DataSet import DataSet, ModeInput
+from NN import NeuralNetwork, ModeLearn
+from ActivFunct import ActivFunct, Sigmoidal, Identity
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing.dummy import Pool as ThreadPool
+from functools import partial
+import random as rnd
+import time
+import threading
+
+
+
+"""
+Funzione per un insieme di iperparametri fissati
+param:
+    k: numero di folder
+    dataset: insieme di trSet+vlSet
+    f: funzione di attivazione
+    theta: dizionario contenente gli iperparametri
+    errorFunct: funzione che stima l'errore
+    modeLearn: Online/MiniBatch/Batch
+    miniBatchDim: dimensione di ciascun minibatch
+"""
+def k_fold_CV_single(k: int, dataSet, f:ActivFunct, theta, errorFunct = None, modeLearn:ModeLearn = ModeLearn.BATCH, miniBatchDim= None):
+    if len(dataSet) % k == 0:
+        rnd.shuffle(dataSet)
+
+        folderDim = int(len(dataSet) / k)
+        folder = [dataSet[i*folderDim : (i+1)*folderDim] for i in range(k)]
+        errore = list()
+        pool = ThreadPool(k)
+        inputList = list()
+        for i in range (len(folder)):
+            lcopy = folder.copy()
+            del(lcopy[i])
+
+            vlSet = folder[i]
+            trSet = list()
+            for j in range (len (lcopy)):
+                trSet+= lcopy[j]
+            nn = NeuralNetwork(trSet, f, theta)
+            inputList.append((nn, vlSet))
+        errore = pool.map(partial(task_cv_single, modeLearn= modeLearn, errorFunct=errorFunct, miniBatchDim=miniBatchDim), inputList)
+        pool.close()
+        pool.join()
+        err = sum(errore)/k
+        return err
+    else:
+        raise ValueError("k must divide data set length")
+
+def task_cv_single(inp, modeLearn: ModeLearn, errorFunct:ActivFunct, miniBatchDim = None):
+    (nn, vlSet) = inp
+    nn.learn(modeLearn, errorFunct, miniBatchDim)
+    e = nn.getError(vlSet, 0, 1/len(vlSet), errorFunct)
+    return e
+
+f = Sigmoidal(8)
+
+domains = [3, 3, 2, 3, 4, 2]
+columnSkip = [8]
+targetPos = 1
+
+trainingSet = DataSet("monks-1.train", " ", ModeInput.ONE_OF_K_TR_INPUT, targetPos, domains, None, columnSkip)
+theta = {'HiddenUnits':4, 'learnRate':0.1, 'ValMax':0.7, 'momRate':0.6, 'regRate':0, 'Tolerance':0.0001, 'MaxEpochs': 200}
+start = time.time()
+e = k_fold_CV_single(4, trainingSet.inputList, f, theta, None,  ModeLearn.BATCH, None)
+stop = time.time() 
+secdiff = stop - start
+print("errore: "+str(e)+" tempo: "+str(secdiff))

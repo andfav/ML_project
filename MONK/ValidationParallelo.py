f1f21b95887a43e6b610687cc48ead66419cdb93
andfav <32015309+andfav@users.noreply.github.com>
2018-12-19 17:49:39 +0100

    Utilizzato multiprocess.Pool

--- MONK/ValidationParallelo.py
+++ MONK/ValidationParallelo.py
@@ -1,11 +1,10 @@
 from DataSet import DataSet, ModeInput
 from NN import NeuralNetwork, ModeLearn
 from ActivFunct import ActivFunct, Sigmoidal, Identity
-from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Pool
 from functools import partial
 import random as rnd
 import time
-import threading
 
 
 
@@ -27,27 +26,28 @@ def k_fold_CV_single(k: int, dataSet, f:ActivFunct, theta, errorFunct = None, mo
         folderDim = int(len(dataSet) / k)
         folder = [dataSet[i*folderDim : (i+1)*folderDim] for i in range(k)]
         errore = list()
-        pool = ThreadPoolExecutor(max_workers=k)
-        futureList = list()
+        pool = Pool(k)
+        poolList = list()
         for i in range (len(folder)):
             lcopy = folder.copy()
             del(lcopy[i])
 
             vlSet = folder[i]
             trSet = list()
-            for j in range (len (lcopy)):
+            for j in range(len(lcopy)):
                 trSet+= lcopy[j]
             nn = NeuralNetwork(trSet, f, theta)
+            poolList.append((nn,vlSet))
+        
+        errore = pool.map(partial(task_cv_single,modeLearn=modeLearn, errorFunct=errorFunct, miniBatchDim=miniBatchDim),poolList)
             
-            futureList.append(pool.submit(partial(task_cv_single, nn=nn, vlSet=vlSet,modeLearn= modeLearn, errorFunct=errorFunct, miniBatchDim=miniBatchDim)))
-        for el in futureList:
-            errore.append(el.result())
         err = sum(errore)/k
         return err
     else:
         raise ValueError("k must divide data set length")
 
-def task_cv_single(nn, vlSet, modeLearn: ModeLearn, errorFunct:ActivFunct, miniBatchDim = None):
+def task_cv_single(t, modeLearn: ModeLearn, errorFunct:ActivFunct, miniBatchDim = None):
+    (nn, vlSet) = t
     nn.learn(modeLearn, errorFunct, miniBatchDim)
     e = nn.getError(vlSet, 0, 1/len(vlSet), errorFunct)
     return e
@@ -59,7 +59,7 @@ columnSkip = [8]
 targetPos = 1
 
 trainingSet = DataSet("monks-1.train", " ", ModeInput.ONE_OF_K_TR_INPUT, targetPos, domains, None, columnSkip)
-theta = {'HiddenUnits':4, 'learnRate':0.1, 'ValMax':0.7, 'momRate':0.6, 'regRate':0, 'Tolerance':0.0001, 'MaxEpochs': 200}
+theta = {'HiddenUnits':4, 'learnRate':0.1, 'ValMax':0.7, 'momRate':0.6, 'regRate':0, 'Tolerance':0.001, 'MaxEpochs': 600}
 start = time.time()
 e = k_fold_CV_single(4, trainingSet.inputList, f, theta, None,  ModeLearn.BATCH, None)
 stop = time.time() 

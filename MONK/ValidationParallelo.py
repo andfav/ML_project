16051918c76e6d8c619529fceab10dde84fb5945
andfav <32015309+andfav@users.noreply.github.com>
2018-12-23 17:29:12 +0100

    Add files via upload

--- MONK/ValidationParallelo.py
+++ MONK/ValidationParallelo.py
@@ -7,6 +7,7 @@ import random as rnd
 import numpy as np
 from numpy.linalg import norm
 import time
+import itertools
 
 
 
@@ -26,39 +27,47 @@ def k_fold_CV_single(workers: int, k: int, dataSet, f:ActivFunct, theta:dict, er
     if workers <= 0:
         workers = 1
 
-    if len(dataSet) % k == 0 or k <= 0:
-        #Rimescolo il data set.
-        rnd.shuffle(dataSet.copy())
+    if k <= 0:
+        raise ValueError ("Wrong value of num. folders inserted")
+    
+    #Rimescolo il data set.
+    rnd.shuffle(dataSet.copy())
 
-        #Creo la lista dei folders.
-        folderDim = int(len(dataSet) / k)
-        folder = [dataSet[i*folderDim : (i+1)*folderDim] for i in range(k)]
+    #Costruisco la sottolista dei dati divisibile esattamente per k.
+    h = len(dataSet) - len(dataSet) % k
+    dataSetExact = dataSet[0:h]
 
-        errore = list()
+    #Creo la lista dei folders.
+    folderDim = int(len(dataSetExact) / k)
+    folders = [dataSet[i*folderDim : (i+1)*folderDim] for i in range(k)]
+
+    #Inserisco gli elementi di avanzo.
+    for i in range(len(dataSet)-h):
+        folders[i].append(dataSet[i+h])
+
+    errore = list()
         
-        poolList = list()
-        with ProcessPoolExecutor(max_workers=min(k, workers)) as pool:
-            for i in range (len(folder)):
-                lcopy = folder.copy()
-                del(lcopy[i])
+    poolList = list()
+    with ProcessPoolExecutor(max_workers=min(k, workers)) as pool:
+        for i in range (len(folders)):
+            lcopy = folders.copy()
+            del(lcopy[i])
 
-                #Creo validation e training set.
-                vlSet = folder[i]
-                trSet = list()
-                for j in range(len(lcopy)):
-                    trSet+= lcopy[j]
+             #Creo validation e training set.
+            vlSet = folders[i]
+            trSet = list()
+            for j in range(len(lcopy)):
+                trSet+= lcopy[j]
                 
-                #Aggiorno la poolList.
-                poolList.append((trSet,vlSet))
+            #Aggiorno la poolList.
+            poolList.append((trSet,vlSet))
         
-            #In parallelo creo, istruisco le reti, calcolo gli errori sui possibili folders.
-            errore = list(pool.map(partial(task_cv_single,modeLearn=modeLearn, f=f, theta=theta, errorFunct=errorFunct, miniBatchDim=miniBatchDim),poolList))
+        #In parallelo creo, istruisco le reti, calcolo gli errori sui possibili folders.
+        errore = list(pool.map(partial(task_cv_single,modeLearn=modeLearn, f=f, theta=theta, errorFunct=errorFunct, miniBatchDim=miniBatchDim),poolList))
 
-            #Restituisco l'errore medio.    
-            err = sum(errore)/k
-        return err
-    else:
-        raise ValueError("k must divide data set length")
+    #Restituisco l'errore medio.    
+    err = sum(errore)/k
+    return err
 
 """
 Task che viene eseguito in parallelo dai processi creati da k_fold_CV_single
@@ -90,47 +99,58 @@ param:
     Tolerance: lista di tutti i possibili valori che il parametro Tolerance della rete neurale può assumere all'interno della cross validation
     miniBatchDim: dimensione di ciascun minibatch (se modeLearn = Minibatch)
 """
-def cross_validation(workers: int, nFolder:int, modeLearn:ModeLearn, dataSet, f:ActivFunct, errorFunct:ActivFunct, learnRate:list, momRate:list, regRate:list, ValMax:list, HiddenUnits:list, OutputUnits:list, MaxEpochs:list, Tolerance:list, miniBatchDim= None):
+def cross_validation(workers: int, nFolder:int, modeLearn:ModeLearn, dataSet, f:ActivFunct, errorFunct:ActivFunct, learnRate:list, momRate:list, regRate:list, ValMax:list, HiddenUnits:list, OutputUnits:int, MaxEpochs:list, Tolerance:list, startTime, miniBatchDim= None):
     if workers <= 0:
         workers = 1
     
     dictionaries = list()
  
     #generazione di tutte le possibili combinazioni
-    for eta in learnRate:
+    for maxVal in ValMax:
         for alfa in momRate:
             for lambd in regRate:
-                for maxVal in ValMax:
+                for eta in learnRate:
                     for hUnit in HiddenUnits:
-                        for oUnit in OutputUnits:
-                            for epochs in MaxEpochs:
-                                for epsilon in Tolerance:
-                                    theta = {'learnRate':eta, 'momRate':alfa, 'regRate':lambd, 'ValMax':maxVal, 'HiddenUnits':hUnit, 'OutputUnits':oUnit, 'MaxEpochs':epochs, 'Tolerance':epsilon}
-                                    dictionaries.append(theta)
+                        for epochs in MaxEpochs:
+                            for epsilon in Tolerance:                                
+                                theta = {'learnRate':eta, 'momRate':alfa, 'regRate':lambd, 'ValMax':maxVal, 'HiddenUnits':hUnit, 'OutputUnits':OutputUnits, 'MaxEpochs':epochs, 'Tolerance':epsilon}
+                                dictionaries.append(theta)
+
+        totalTheta = len(dictionaries)
+        print("Configurazioni totali di iperparametri: "+str(totalTheta))
 
         res = list()
         future = list()
-        with ProcessPoolExecutor(max_workers=workers) as pool:
+        with ProcessPoolExecutor(max_workers=6) as pool:
             for theta in dictionaries:
-                future.append(pool.submit(partial(cross_validation_iterator, workers=2, nFolder=nFolder, dataSet=dataSet.copy(), f=f, theta=theta, errorFunct=errorFunct, modeLearn=modeLearn, miniBatchDim=miniBatchDim)))
+                future.append(pool.submit(partial(cross_validation_iterator, nIter=10, workers=workers, nFolder=nFolder, dataSet=dataSet.copy(), f=f, theta=theta, startTime=startTime, errorFunct=errorFunct, modeLearn=modeLearn, miniBatchDim=miniBatchDim)))
 
             for elem in future:
                 res.append(elem.result())
 
     return res
     
-def cross_validation_iterator(workers: int, nFolder: int, dataSet, f:ActivFunct, theta:dict, errorFunct = None, modeLearn:ModeLearn = ModeLearn.BATCH, miniBatchDim= None, nIter: int = 10):
+def cross_validation_iterator(workers: int, nFolder: int, dataSet, f:ActivFunct, theta:dict, startTime, errorFunct = None, modeLearn:ModeLearn = ModeLearn.BATCH, miniBatchDim= None, nIter: int = 10):
     future = list()
-    with ProcessPoolExecutor(max_workers=min(workers, nIter)) as pool:
+    with ProcessPoolExecutor(max_workers=min(workers,nIter)) as pool:
         for i in range(0, nIter):
-            future.append(pool.submit(partial(k_fold_CV_single, workers=2, k=nFolder, dataSet=dataSet, modeLearn=modeLearn, f=f, theta=theta, errorFunct=errorFunct, miniBatchDim=miniBatchDim)))
+            future.append(pool.submit(partial(k_fold_CV_single, workers, k=nFolder, dataSet=dataSet, modeLearn=modeLearn, f=f, theta=theta, errorFunct=errorFunct, miniBatchDim=miniBatchDim)))
 
         
         errore = list()
         for elem in future:
             errore.append(elem.result())
 
+    endTime = time.time()
+    print("************")
+    print("Configurazione completata, tempo: " + str(endTime - startTime))
+    print(str((theta, sum(errore)/nIter)))
+
     return (theta, sum(errore)/nIter)
+
+def getBestResult(e):
+    l = [e[i][1] for i in range(len(e))]
+    return e[l.index(min(l))]
 
 if __name__ == '__main__':
     f = Sigmoidal(8)
@@ -138,11 +158,21 @@ if __name__ == '__main__':
     domains = [3, 3, 2, 3, 4, 2]
     columnSkip = [8]
     targetPos = 1
+    datasetFileName = "monks-1.train"
+    logFileName = datasetFileName + ".log"
 
-    trainingSet = DataSet("monks-2.train", " ", ModeInput.ONE_OF_K_TR_INPUT, targetPos, domains, None, columnSkip)
+    trainingSet = DataSet(datasetFileName, " ", ModeInput.ONE_OF_K_TR_INPUT, targetPos, domains, None, columnSkip)
     theta = {'HiddenUnits':4, 'learnRate':0.1, 'ValMax':0.7, 'momRate':0.6, 'regRate':0, 'Tolerance':0.037, 'MaxEpochs': 600}
     start = time.time()
-    e = cross_validation(2, 13, ModeLearn.BATCH, trainingSet.getInputs(), f, None, [0.1, 0.01], [0.6, 0.5], [0], [0.7], [2, 3, 4], [1], [500], [0.0001], None)
+    e = cross_validation(60, 8, ModeLearn.BATCH, trainingSet.getInputs(), f, None, [0.1, 0.075, 0.05, 0.025, 0.01], [0.7, 0.6, 0.5, 0.4, 0.3], [0.01, 0.005, 0], [0.7], [2, 3, 4, 5], 1, [1000], [0.006], start, None)
     stop = time.time() 
     secdiff = stop - start
-    print("errore: "+str(e)+" tempo: "+str(secdiff))
+
+    log = open(logFileName,'a')
+    log.write("***\n")
+    log.write(str(e) + '\n')
+    log.write('\n')
+    log.write("Miglior risultato: " + str(getBestResult(e)) + '\n')
+    log.write('\n\n')
+
+    print("Operazione di cross-validation conclusa in " + str(secdiff) + " secondi: i dati sono stati salvati in " + logFileName + ".")
